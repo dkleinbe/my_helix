b2edd2581cf0d85e8269737cfd10b31a5651b9f7
dkleinbe_vscode <8706463+dkleinbe@users.noreply.github.com>
2026-07-17 10:44:02 +0200

    logtape added file source for middleware messages

--- server/src/tools/tapeLogger.ts
+++ server/src/tools/tapeLogger.ts
@@ -13,7 +13,8 @@ import {
   parseLogLevel,
   lazy
 } from "@logtape/logtape";
-import { log } from "node:console";
+import { prettyFormatter } from "@logtape/pretty";
+
 
 export async function configureLogger() {
 
@@ -22,28 +23,6 @@ export async function configureLogger() {
         // @ts-ignore
         stream: getStreamSink(stream.Writable.toWeb(process.stderr), {formatter: ansiColorFormatter}),
         
-        my_console: getConsoleSink({
-        formatter(record: LogRecord): readonly unknown[] {
-            let msg = "";
-            const values: unknown[] = [];
-            for (let i = 0; i < record.message.length; i++) {
-            if (i % 2 === 0) msg += record.message[i];
-            else {
-                msg += "%o";
-                values.push(record.message[i]);
-            }
-            }
-            return [
-            `${record.level.toUpperCase()} %c${
-                record.category.join("\xb7")
-            } %c${msg}`,
-            "color: gray;",
-            "color: default;",
-            ...values,
-            ];
-        }
-        }),    
-
         my_console3: getConsoleSink({
             formatter: (record: LogRecord) => { 
                 //console.log(filename(9))
@@ -54,12 +33,14 @@ export async function configureLogger() {
                 return str
             }
         }),    
+        console2: getConsoleSink({formatter: prettyFormatter}),
         console: getConsoleSink({formatter: ansiColorFormatter}),
     },
     loggers: [
-        { category: ["logtape", "meta"], sinks: ["console"] },
-        { category: "my-app", sinks: ["console"], lowestLevel: "info",  },
-        { category: ["my-express"], sinks: ["console"], lowestLevel: "info" },
+        { category: ["logtape", "meta"], sinks: ["console2"] },
+        { category: "my-app", sinks: ["console2"], lowestLevel: "info",  },
+        { category: ["my-express"], sinks: ["console2"], lowestLevel: "error" },
+        { category: ["my-express", "middleware"], sinks: ["console2"], parentSinks: "override", lowestLevel: "info" },
         //{ category: ["express"], sinks: ["my_console3"], lowestLevel: "info" },
     ],
     contextLocalStorage: new AsyncLocalStorage(),
@@ -69,7 +50,7 @@ export async function configureLogger() {
 
 export const logApp = getLogger(["my-app"]);
 
-const filename = function getCallerFile(position = 4) {
+const filename = function getCallerFile(position = 4) : string {
   if (position >= Error.stackTraceLimit) {
     throw new TypeError('getCallerFile(position) requires position be less then Error.stackTraceLimit but position was: `' + position + '` and Error.stackTraceLimit was: `' + Error.stackTraceLimit + '`');
   }
@@ -84,14 +65,15 @@ const filename = function getCallerFile(position = 4) {
     // stack[0] holds this file
     // stack[1] holds where this function was called
     // stack[2] holds the file we're interested in
-    return stack[position] ? (stack[position] as any).getFileName() + ':' + (stack[position] as any).getLineNumber() : undefined;
+    return stack[position] ? (stack[position] as any).getFileName() + ':' + (stack[position] as any).getLineNumber() : 'unknown';
   }
+  return 'unknown'
 };
 
 
-
+type LocalStore = { message: string; filename: string }
 class LoggerBuilder {
-  private storage: AsyncLocalStorage<{ message: string; }>;
+  private storage: AsyncLocalStorage<{ message: string; filename: string }>;
 
   constructor() {
     console.log(">>>>>>>>>>>>>>>>>  LogBuilder")
@@ -101,33 +83,34 @@ class LoggerBuilder {
     this.log = this.log.bind(this);
   }
 
-  expressLog = getLogger("my-express")
+  expressLog = getLogger( ["my-express", "middleware"])
+  requestLog = getLogger( ["my-express"])
 
   appLog = getLogger("my-app")
 
   middleware(req: Request, res: Response, next: NextFunction) {
     const startTime = Date.now();
-    this.log('debug', req.method, req.originalUrl, res.statusCode, 0, 'Request received');
+    this.log('debug', req.method, req.originalUrl, res.statusCode, 0, { message: 'Request received', filename: 'no file'});
     //this.tapeLog.error("{method} {url}", {method: req.method, url: req.originalUrl})
 
-    this.storage.run({ message: 'Request Completed' }, () => {
+    this.storage.run({ message: 'Request Completed', filename: 'je sais pas' }, () => {
       res.on('finish', () => {
         const responseTime = Date.now() - startTime;
-        const message = this.storage.getStore();
+        const store = this.storage.getStore();
         const level = res.statusCode < 400 ? 'info' : 'error';
-        if (message)
+        if (store?.message)
         {
-          this.log(level, req.method, req.originalUrl, res.statusCode, responseTime, message.message);
+          this.log(level, req.method, req.originalUrl, res.statusCode, responseTime, store);
         }
         else
         {
-          this.log(level, req.method, req.originalUrl, res.statusCode, responseTime, 'no message');
+          this.log(level, req.method, req.originalUrl, res.statusCode, responseTime, { message: 'no msg', filename: 'no file'});
         }
       });
 
       res.on('error', (err) => {
         const responseTime = Date.now() - startTime;
-        this.log('error', req.method, req.originalUrl, res.statusCode, responseTime, 'Error occurred:', err.message);
+        this.log('error', req.method, req.originalUrl, res.statusCode, responseTime, { message: err.message, filename: 'no file'});
       });
 
       // res.on('close', () => {
@@ -138,11 +121,18 @@ class LoggerBuilder {
     });
   };
 
-  public log = (level: string, method: string, url: string, statusCode: number, responseTime: number|undefined,  ...message: string[]) => {
+  private log = (level: string, method: string, url: string, statusCode: number, responseTime: number|undefined,  store: LocalStore) => {
+
+    //const msg = store.message.toString()
+    this.expressLog[parseLogLevel(level)]("{method} {url} {statusCode} - {responseTime} ms \x1b[34m(\x1b[0m{message}\x1b[34m)\x1b[0m - {filename}", 
+        {method: method, url: url, statusCode: statusCode, responseTime, message: store.message, filename: store.filename})
+  };
+
+  private reqLog = (level: string, method: string, url: string, statusCode: number, responseTime: number|undefined,  ...message: string[]) => {
 
     const msg = message.toString()
-    this.expressLog[parseLogLevel(level)]("{method} {url} {statusCode} - {responseTime} ms \x1b[34m(\x1b[0m{message}\x1b[34m)\x1b[0m", 
-        {method: method, url: url, statusCode: statusCode, responseTime, message: msg})
+    this.requestLog[parseLogLevel(level)]("{method} {url} {statusCode} - {responseTime} ms \x1b[34m(\x1b[0m{message}\x1b[34m)\x1b[0m - {filename}", 
+        {method: method, url: url, statusCode: statusCode, responseTime, message: msg, filename: filename(3)})
   };
 
   private appLogger = (level: string, message: string) => {
@@ -162,16 +152,14 @@ class LoggerBuilder {
   }
 
   successReq = (req: Request, res: Response, ...message: string[]) => {
-    this.log('debug', req.method, req.originalUrl, res.statusCode, 0, ...message)
+    this.reqLog('info', req.method, req.originalUrl, res.statusCode, 0, ...message)
   }  
   failReq = (req: Request, res: Response, ...message: string[]) => {
-    this.log('warning', req.method, req.originalUrl, res.statusCode, 0, ...message)
-  }  
-  errorReq = (message: string) => {
-    this.appLogger('error', message)
+    this.reqLog('warning', req.method, req.originalUrl, res.statusCode, 0, ...message)
   }
+
   message(msg: string) {
-    this.storage.enterWith({ message: msg });
+    this.storage.enterWith({ message: msg, filename: filename(3) });
   }
 }
 //export const log = new LoggerBuilder()
